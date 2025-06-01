a6a458eb01d1cb142d1c2cfe9617be97953d8da1
Rodion <korsun.rodion@gmail.com>
2025-06-01 14:49:56 +0300

    Summer changes

--- data/projects.ts
+++ data/projects.ts
@@ -12,23 +12,75 @@ export interface IProject extends IProjectData {
 
 const Projects: IProject[] = [{
   id: 1,
-  title: 'Supreme Court of Ukraine [in test]',
+  title: 'Supreme Court of Ukraine\'s Observer',
   stack: 'React, Next.js, Strapi, Elasticsearch',
-  link: 'http://supreme-test.site/',
+  link: 'https://so.supreme.court.gov.ua/',
   img: '/images/supreme.webp',
-}, {
+},
+{
+  id: 7,
+  title: 'CD Keys',
+  stack: 'React, Node.js, Express',
+  link: 'https://cdkeys.com/',
+  img: '/images/cd_keys.webp',
+},
+{
   id: 0,
   title: 'Andoni Law',
   stack: 'React, Next.js, Strapi',
   link: 'https://andonilaw.com/',
   img: '/images/andoni2.webp',
-}, {
+},
+{
+  id: 8,
+  title: 'XGateway',
+  stack: 'React, Next.js, Node.js, Express',
+  link: 'https://xgateway.tech/',
+  img: '/images/xgateway.webp',
+},
+{
+  id: 12,
+  title: 'LiveTicketGroup',
+  stack: 'React, Next.js, Python, Flask',
+  link: 'https://www.livefootballtickets.com/',
+  img: '/images/ltg.webp',
+},
+{
+  id: 10,
+  title: 'Garantme',
+  stack: 'React, Next.js, Python, Flask',
+  link: 'https://garantme.com/',
+  img: '/images/garantme.webp',
+},
+{
+  id: 9,
+  title: 'Echoes',
+  stack: 'React, Next.js, Node.js, NestJS',
+  link: 'https://echoes.xyz/',
+  img: '/images/echoes.webp',
+},
+{
   id: 3,
   title: 'Smotrow Related',
   stack: 'Vue, Nuxt, Laravel, Laravel Nova',
   link: 'https://smotrowrelated.com/',
   img: '/images/smotrowrelated.webp',
-}, {
+},
+{
+  id: 11,
+  title: 'Lorgar',
+  stack: 'React, Next.js, Node.js, Express',
+  link: 'https://lorgar.com/',
+  img: '/images/lorgar.webp',
+},
+{
+  id: 12,
+  title: 'Proto',
+  stack: 'React, Next.js, Node.js, Express',
+  link: 'https://www.protosimulator.com/',
+  img: '/images/proto.webp',
+},
+{
   id: 4,
   title: 'Justnote',
   stack: 'Angular',
--- pages/api/chat-send.ts
+++ pages/api/chat-send.ts
@@ -1,7 +1,12 @@
 import { IMessage } from '@/hooks/useChat';
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import OpenAI from 'openai';
 import fs from 'fs';
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_TOKEN,
+});
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
@@ -16,116 +21,74 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Transfer-Encoding', 'chunked');
-  res.setHeader('Content-Encoding', 'none');
-  res.flushHeaders();
+  const messages: OpenAI.Chat.ChatCompletionMessageParam[] = [{
+    role: 'system',
+    content: `
+    You are an AI assistant representing Rodion Korsun. Your task is to respond on Rodion's behalf,
+    in polite, precise, and informal manner. You should respond to any question or message
+    not about Rodion normally, and respond with information that could potentially help Rodion get hired by
+    the user when asked about Rodion. Keep the responses simple.\n\n
+    Rodion has experience with following technologies: React,Vue,Next.js,Redux,Mobx,Sass,Less,
+    React-spring,Gsap,Playwright,Jest,Storybook,AWS,Firebase,Heroku,Flask,Django,Node.js,Express,
+    Spring stack,SQL,PHP,Laravel. You, the Assistant, can assume that Rodion is familiar with any front-end library, but suggest to ask Rodion directly in response.\n\n
+    When user wants to contact Rodion, use can respond using #TEL# for Telegram app, without @ symbol, and #MAIL# for email variables.
+    You can suggest to take a message for Rodion. When user wants you to pass message for Rodion,
+    you add word #REQ# every time, in ALL cases, at the end of the response, but only when user sent their message with contact info.
+    If asked to write a poem, do not write it about Rodion.
+  `,
+  }, {
+    role: 'user',
+    content: `
+      You are an AI assistant representing Rodion Korsun. Your task is to respond on Rodion's behalf,
+      in polite, precise, and informal manner. You should respond to any question or message
+      not about Rodion normally, and respond with information that could potentially help Rodion get hired by
+      the user when asked about Rodion. Keep the responses simple.\n\n
+      Rodion has experience with following technologies: React,Vue,Next.js,Redux,Mobx,Sass,Less,
+      React-spring,Gsap,Playwright,Jest,Storybook,AWS,Firebase,Heroku,Flask,Django,Node.js,Express,
+      Spring stack,SQL,PHP,Laravel. You, the Assistant, can assume that Rodion is familiar with any front-end library, but suggest to ask Rodion directly in response.\n\n
+      When user wants to contact Rodion, use can respond using #TEL# for Telegram app, without @ symbol, and #MAIL# for email variables.
+      You can suggest to take a message for Rodion. When user wants you to pass message for Rodion,
+      you add word #REQ# every time, in ALL cases, at the end of the response, but only when user sent their message with contact info.
+      If asked to write a poem, do not write it about Rodion.
+    `,
+  },
+  ...body.map((item) => ({
+    role: item.from === 'user' ? 'user' : 'assistant',
+    content: item.text,
+  })) as OpenAI.Chat.ChatCompletionMessageParam[],
+  ];
 
-  const requestBody = {
-    model: 'gpt-4',
-    stream: true,
-    messages: [{
-      role: 'system',
-      content: `
-        You are an AI assistant representing Rodion Korsun. Your task is to respond on Rodion's behalf,
-        in polite, precise, and informal manner. You should respond to any question or message
-        not about Rodion normally, and respond with information that could potentially help Rodion get hired by
-        the user when asked about Rodion. Keep the responses simple.\n\n
-        Rodion has experience with following technologies: React,Vue,Next.js,Redux,Mobx,Sass,Less,
-        React-spring,Gsap,Playwright,Jest,Storybook,AWS,Firebase,Heroku,Flask,Django,Node.js,Express,
-        Spring stack,SQL,PHP,Laravel. You, the Assistant, can assume that Rodion is familiar with any front-end library, but suggest to ask Rodion directly in response.\n\n
-        When user wants to contact Rodion, use can respond using #TEL# for Telegram app, without @ symbol, and #MAIL# for email variables.
-        You can suggest to take a message for Rodion. When user wants you to pass message for Rodion,
-        you add word #REQ# every time, in ALL cases, at the end of the response, but only when user sent their message with contact info.
-        If asked to write a poem, do not write it about Rodion.
-      `,
-    }, {
-      role: 'user',
-      content: `
-        You are an AI assistant representing Rodion Korsun. Your task is to respond on Rodion's behalf,
-        in polite, precise, and informal manner. You should respond to any question or message
-        not about Rodion normally, and respond with information that could potentially help Rodion get hired by
-        the user when asked about Rodion. Keep the responses simple.\n\n
-        Rodion has experience with following technologies: React,Vue,Next.js,Redux,Mobx,Sass,Less,
-        React-spring,Gsap,Playwright,Jest,Storybook,AWS,Firebase,Heroku,Flask,Django,Node.js,Express,
-        Spring stack,SQL,PHP,Laravel. You, the Assistant, can assume that Rodion is familiar with any front-end library, but suggest to ask Rodion directly in response.\n\n
-        When user wants to contact Rodion, use can respond using #TEL# for Telegram app, without @ symbol, and #MAIL# for email variables.
-        You can suggest to take a message for Rodion. When user wants you to pass message for Rodion,
-        you add word #REQ# every time, in ALL cases, at the end of the response, but only when user sent their message with contact info.
-        If asked to write a poem, do not write it about Rodion.
-      `,
-    }, ...body.map((item) => ({
-      role: item.from === 'user' ? 'user' : 'assistant',
-      content: item.text,
-    }))],
-  };
+  const response = await client.chat.completions.create({
+    model: 'gpt-4o',
+    messages,
+  });
 
-  const config: AxiosRequestConfig = {
-    headers: {
-      Authorization: `Bearer ${process.env.OPENAI_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-  };
+  let message = response.choices[0].message.content;
 
-  const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, config);
-
-  let command = '';
-  let waitingForCommand = false;
-  let returnText = '';
-
-  const messages = response.data.toString().trim().split('\n\n').map((item: string) => item.slice(6));
-  console.log(messages);
-  for (const i of messages) {
-    if (i === '[DONE]') {
-      break;
-    }
-    const json = JSON.parse(i.replace(/\n/g, ''));
-    if (json.choices.length > 0 && json.choices[0]?.delta?.content) {
-      let responseText = json.choices[0].delta.content;
-
-      if (waitingForCommand) {
-        const pos = responseText.indexOf('#');
-        if (pos === -1) {
-          command += responseText;
-        } else {
-          command += responseText.slice(0, pos);
-        }
+  if (message?.includes('#REQ#')) {
+    message = message.replace('#REQ#', '');
+    axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: '376907585',
+      text: JSON.stringify(body),
+    }).then((telegramResponse) => {
+      if (telegramResponse.status !== 200) {
+        fs.appendFile('logs.txt', JSON.stringify(body), (err) => {
+          console.log('Error occured writing logs', err);
+        });
       }
-
-      if (responseText.includes('#')) {
-        if (waitingForCommand) {
-          if (command === 'REQ') {
-            responseText = '';
-            axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
-              chat_id: '376907585',
-              text: JSON.stringify(body),
-            }).then((telegramResponse) => {
-              if (telegramResponse.status !== 200) {
-                fs.appendFile('logs.txt', JSON.stringify(body), (err) => {
-                  console.log('Error occured writing logs', err);
-                });
-              }
-            }).catch(() => {
-              fs.appendFile('logs.txt', JSON.stringify(body), (err) => {
-                console.log('Error occured writing logs', err);
-              });
-            });
-          } else if (command === 'TEL') {
-            responseText = '{telegram}';
-          } else if (command === 'MAIL') {
-            responseText = '{email}';
-          }
-          command = '';
-        }
-        waitingForCommand = !waitingForCommand;
-      }
-      if (!waitingForCommand) {
-        returnText += responseText;
-      }
-    }
+    }).catch(() => {
+      fs.appendFile('logs.txt', JSON.stringify(body), (err) => {
+        console.log('Error occured writing logs', err);
+      });
+    });
   }
-  res.write(returnText);
+  if (message?.includes('#TEL#')) {
+    message = message.replace('#TEL#', 'https://t.me/korsunrodion');
+  }
+  if (message?.includes('#MAIL#')) {
+    message = message.replace('#MAIL#', 'korsun.rodion@gmail.com');
+  }
+
+  res.write(message);
   res.status(200).end();
 };
